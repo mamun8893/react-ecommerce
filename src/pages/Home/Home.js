6f064ddd2af0423b6fcc3adbd7ec9e4b5de474b9
mamun8893 <0316361936@student.wub.edu.bd>
2022-01-15 00:53:41 +0600

    menu item complete

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory";
+import "./home.scss";
+const Home = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Home;

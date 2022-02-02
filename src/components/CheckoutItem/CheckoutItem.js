982fa374e7f3b68290f1ca7dba73b6b46f47a3af
mamun8893 <0316361936@student.wub.edu.bd>
2022-02-03 00:29:21 +0600

    remove cart

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -13,7 +13,7 @@ const CheckoutItem = ({ cartItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
-      <span className="price">{price}</span>
+      <span className="price">${price}</span>
       <div
         className="remove-button"
         onClick={() => dispatch(removeCartAction(cartItem))}

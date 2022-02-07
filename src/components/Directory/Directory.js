b97652d02e8f8b851bc700f9631aa0b07dc6f921
mamun8893 <0316361936@student.wub.edu.bd>
2022-02-07 22:00:43 +0600

    directory state add custom hooks

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,47 +1,11 @@
 import React, { useState } from "react";
+import useSection from "../../hooks/useSection";
 import MenuItem from "../MenuItem/MenuItem";
 import "./directory-menu.scss";
 
 const Directory = () => {
-  const [section, setSection] = useState([
-    {
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      id: 1,
-      linkUrl: "shop/hats",
-      link: "hats",
-    },
-    {
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      id: 2,
-      linkUrl: "shop/jackets",
-      link: "",
-    },
-    {
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      id: 3,
-      linkUrl: "shop/sneakers",
-      link: "",
-    },
-    {
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-      id: 4,
-      linkUrl: "shop/womens",
-      link: "",
-    },
-    {
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      id: 5,
-      linkUrl: "shop/mens",
-      link: "",
-    },
-  ]);
+  const [section] = useSection();
+
   return (
     <div className="directory-menu">
       {section.map(({ title, imageUrl, size, id, link }) => (

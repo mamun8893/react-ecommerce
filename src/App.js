0b8f9288f4146c1f0f13bb8a931529901a053f1c
mamun8893 <0316361936@student.wub.edu.bd>
2022-02-19 19:12:51 +0600

    feat: lazy load add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.scss";
-import Home from "./pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Shop from "./pages/Shop/Shop";
 import Header from "./components/Header/Header";
-import Auth from "./pages/Auth/Auth";
 import AuthProvider from "./context/AuthProvider";
-import Checkout from "./components/Checkout/Checkout";
-import Collection from "./pages/Collection/Collection";
+import Spinner from "./components/Spinner/Spinner";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Shop = lazy(() => import("./pages/Shop/Shop"));
+const Auth = lazy(() => import("./pages/Auth/Auth"));
+const Checkout = lazy(() => import("./components/Checkout/Checkout"));
+const Collection = lazy(() => import("./pages/Collection/Collection"));
 
 function App() {
   return (
@@ -15,14 +17,17 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="shop" element={<Shop />}>
-              <Route path=":collectionName" element={<Collection />} />
-            </Route>
-            <Route path="signup" element={<Auth />}></Route>
-            <Route path="checkout" element={<Checkout />}></Route>
-          </Routes>
+          <Suspense fallback={<Spinner />}>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+
+              <Route path="shop" element={<Shop />}>
+                <Route path=":collectionName" element={<Collection />} />
+              </Route>
+              <Route path="signup" element={<Auth />}></Route>
+              <Route path="checkout" element={<Checkout />}></Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </AuthProvider>
     </div>
